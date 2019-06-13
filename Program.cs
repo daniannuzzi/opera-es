c75ce2fc39aaa15b3da68fc556f523b822768e29
Danielle Iannuzzi <danielle.iannuzzi@hotmail.com>
2019-06-13 12:20:11 -0300

    Criar interação com usuário e criação de 5 operações matemáticas

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace QuatroOperacoes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaração das variáveis
+            /*
+            Vamos declarar as sete variáveis, sendo duas para os usuários inserir dados e as outras serão para armazenar os resultados
+             */
+            
+             int numero1, numero2;
+             int soma, subtrair, multiplicar, dividir,resto;
+            
+            //  Pedir ao usuários que entre com o primeiro número
+            Console.WriteLine("Digite um número:");
+
+            // Vamos pegar o valor que o usuário digitou e alocar na variável número1
+
+            // Utilizamos o comando int.Parse para passar a captura de texto do console.readline
+            // em número mesmo sabendo que o usuário irá digitar um número, o readline sempre lê como texto
+    
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número:");
+
+            numero2 = int.Parse(Console.ReadLine());
+
+            // Abaixo iremos realizar as 5 operações aritméticas
+
+            soma = numero1+numero2; 
+            subtrair = numero1-numero2;
+            dividir = numero1/numero2;
+            multiplicar = numero1*numero2;
+            resto = numero1%numero2;
+            
+             
+
         }
     }
 }
